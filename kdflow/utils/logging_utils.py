a63e78d98e9b6a70ae16d3cf4062a110a9505d19
songmzhang <787073341@qq.com>
2026-03-16 22:49:25 +0800

    style: colorize logs

--- kdflow/utils/logging_utils.py
+++ kdflow/utils/logging_utils.py
@@ -1,18 +1,31 @@
 import logging
 import sys
 
-_FORMAT = "[%(asctime)s] [%(levelname)s] [%(module)s:%(funcName)s:%(lineno)d] %(message)s"
+_FORMAT = "[%(asctime)s] [%(levelname)s] [%(filename)s:%(funcName)s:%(lineno)s] %(message)s"
 _DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
+_LEVEL_COLORS = {"DEBUG": "36", "INFO": "32", "WARNING": "33", "ERROR": "31", "CRITICAL": "1;31"}
+_DIM = "\033[2m"
+_RESET = "\033[0m"
 
-class NewLineFormatter(logging.Formatter):
-    """Adds logging prefix to newlines to align multi-line messages."""
 
-    def __init__(self, fmt, datefmt=None):
-        logging.Formatter.__init__(self, fmt, datefmt)
+class ColoredNewLineFormatter(logging.Formatter):
+    """Colored formatter that also aligns multi-line messages."""
 
     def format(self, record):
-        msg = logging.Formatter.format(self, record)
+        c = _LEVEL_COLORS.get(record.levelname)
+        if c:
+            saved = record.levelname, record.filename, record.funcName, record.lineno
+            bold = "1;" if record.levelname == "CRITICAL" else ""
+            record.levelname = f"\033[{bold}{c}m{record.levelname}{_RESET}"
+            record.filename = f"{_DIM}{record.filename}"
+            record.funcName = f"{record.funcName}"
+            record.lineno = f"{saved[3]}{_RESET}"
+            msg = super().format(record)
+            record.levelname, record.filename, record.funcName, record.lineno = saved
+        else:
+            msg = super().format(record)
+
         if record.message != "":
             parts = msg.split(record.message)
             msg = msg.replace("\n", "\r\n" + parts[0])
@@ -31,7 +44,7 @@ def _setup_logger():
         _default_handler.flush = sys.stdout.flush  # type: ignore
         _default_handler.setLevel(logging.INFO)
         _root_logger.addHandler(_default_handler)
-    fmt = NewLineFormatter(_FORMAT, datefmt=_DATE_FORMAT)
+    fmt = ColoredNewLineFormatter(_FORMAT, datefmt=_DATE_FORMAT)
     _default_handler.setFormatter(fmt)
     # Setting this will avoid the message
     # being propagated to the parent logger.
